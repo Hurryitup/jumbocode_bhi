296ee96207b11ba2ad477c54cf10089ed46d7323
Jenna Kubiak <jennakubiak@Jennas-MacBook-Pro-3.local>
2016-04-10 21:57:43 -0400

    responsive buttons

--- index.js
+++ index.js
@@ -16,22 +16,57 @@ $(document).ready(function(){
     $("#start_man").keyup(function (e) {
         if (e.which == 13) {
             start = $("#start_man").val();
+            document.getElementById("start_title").innerHTML = start;
         }
 	});
-	$("#start_Boston").click(function(){ start = "Boston"; });
-	$("#start_Hull").click(function(){ start = "Hull"; });
-	$("#start_Hingham").click(function(){ start = "Hingham"; });
-	$("#Georges").click(function(){ stops[num_islands] = "Georges Island"; });
-	$("#Spectacle").click(function(){ stops[num_islands] = "Spectacle Island"; });
-	$("#Peddocks").click(function(){ stops[num_islands] = "Peddocks"; });
-	$("#1030").click(function(){ time[num_islands] = "10:30"; });
-	$("#1200").click(function(){ time[num_islands] = "12:00"; });
+	$("#start_Boston").click(function(){ 
+		start = "Boston";
+		document.getElementById("start_title").innerHTML = start;
+	});
+	$("#start_Hull").click(function(){ 
+		start = "Hull";
+		document.getElementById("start_title").innerHTML = start;		 
+	});
+	$("#start_Hingham").click(function(){ 
+		start = "Hingham"; 
+		document.getElementById("start_title").innerHTML = start;
+	});
+	$("#Georges").click(function(){ 
+		stops[num_islands] = "Georges Island"; 
+		document.getElementById("island_title").innerHTML = "Georges";		
+	});
+	$("#Spectacle").click(function(){ 
+		stops[num_islands] = "Spectacle Island"; 
+		document.getElementById("island_title").innerHTML = "Spectacle";
+	});
+	$("#Peddocks").click(function(){ 
+		stops[num_islands] = "Peddocks";
+		document.getElementById("island_title").innerHTML = "Peddocks"; 
+	});
+	$("#1030").click(function(){ 
+		time[num_islands] = "10:30"; 
+		document.getElementById("time_title").innerHTML = time[num_islands];
+	});
+	$("#1200").click(function(){ 
+		time[num_islands] = "12:00"; 
+		document.getElementById("time_title").innerHTML = time[num_islands];		
+	});
 	$("#finish_man").keyup(function (e) {
 		if (e.which == 13) {
-    		start = $("#finish_man").val();
+    		finish = $("#finish_man").val();
+    		document.getElementById("finish_title").innerHTML = finish;
 		}
     });
-    $("#end_Boston").click(function(){ finish = "Boston"; });
-    $("#end_Hull").click(function(){ finish = "Hull"; });
-    $("#end_Hingham").click(function(){ finish = "Hingham"; });
+    $("#end_Boston").click(function(){ 
+    	finish = "Boston"; 
+    	document.getElementById("finish_title").innerHTML = finish;
+    });
+    $("#end_Hull").click(function(){ 
+    	finish = "Hull"; 
+    	document.getElementById("finish_title").innerHTML = finish;
+    });
+    $("#end_Hingham").click(function(){ 
+    	finish = "Hingham"; 
+    	document.getElementById("finish_title").innerHTML = finish;
+    });
 });
